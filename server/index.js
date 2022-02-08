a34d7092f1325031c7d55abc867b7a7cfda85283
Dallin Breen <dallinbreen624@gmail.com>
2022-02-08 14:38:12 -0700

    Finished back end practice

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const ctrl = require("./controller");
+const app = express();
+const port = 4004;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/houses", ctrl.getHouses);
+app.delete("/api/houses/:id", ctrl.deleteHouse);
+app.post("/api/houses", ctrl.createHouse);
+app.put("/api/houses/:id", ctrl.updateHouse);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
